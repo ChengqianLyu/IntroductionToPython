455be8ca6bf411803eae1cbbaf49f8011a773702
ChengqianLyu <lyuc@rose-hulman.edu>
2017-08-31 13:30:14 -0400

    DONE

--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -2,15 +2,40 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Chengqian Lyu HERE.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 # On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
+import rosegraphics as rg
+window=rg.TurtleWindow
+matttt=rg.SimpleTurtle('turtle')
+matttt.pen=rg.Pen('red',25)
+matttt.speed=5
+matttt.forward(120)
+matttt.right(120)
+matttt.forward(240)
+matttt.right(120)
+matttt.forward(240)
+matttt.right(120)
+matttt.forward(120)
+mattt=rg.SimpleTurtle('turtle')
+mattt.pen=rg.Pen('red',25)
+mattt.speed=5
+mattt.backward(20)
+mattt.right(120)
+mattt.forward(180)
+mattt.left(120)
+mattt.forward(240)
+mattt.left(120)
+mattt.forward(240)
+mattt.left(120)
+mattt.forward(60)
 
+window.close_on_mouse_click()
 ########################################################################
-# TODO: 2.
+# DONE: 2.
 #
 #  You should have RUN the PREVIOUS module and READ its code.
 #  (Do so now if you have not already done so.)
